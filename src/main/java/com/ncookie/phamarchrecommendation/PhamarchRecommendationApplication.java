23c3cbfaf0bb962fbb7a1ab9b1a080c394989540
SeungWoo Ryu <swstar21c@gmail.com>
2024-12-13 17:38:58 +0900

    first commit

---
+++ src/main/java/com/ncookie/phamarchrecommendation/PhamarchRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.ncookie.phamarchrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamarchRecommendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhamarchRecommendationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncookie/phamarchrecommendation/PhamarchRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncookie.phamarchrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamarchRecommendationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
