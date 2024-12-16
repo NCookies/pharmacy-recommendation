5aa1181e7054755d094dd48364eb791d4218ac58
SeungWoo Ryu <swstar21c@gmail.com>
2024-12-16 16:08:57 +0900

    패키지 이름 변경
    - 프로젝트 이름이 잘못 설정되어 있어 이름을 수정했다.

--- src/test/java/com/ncookie/pharmacy_recommendation/PhamarchRecommendationApplicationTests.java
+++ src/test/java/com/ncookie/pharmacy_recommendation/PhamarchRecommendationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ncookie.phamarchrecommendation;
+package com.ncookie.pharmacy_recommendation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
