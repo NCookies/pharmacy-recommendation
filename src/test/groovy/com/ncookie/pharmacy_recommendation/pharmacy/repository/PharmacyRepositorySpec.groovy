334cc86bfbd20e17984389ba3a964984e6d4dec2
SeungWoo Ryu <swstar21c@gmail.com>
2024-12-19 01:03:36 +0900

    Testcontainers를 이용한 통합 테스트 환경 구성
    - 테스트 시 도커 컨테이너를 코드에서 컨트롤할 수 있도록 해주는 Testcontainers 라이브러리를 사용해 통합 테스트 환경을 구성했다.
    - 테스트 메소드마다 컨테이너를 실행시켰다 껐다를 반복하면 비효율적이므로 싱글톤 방식으로 구현했다.
    - Spock 프레임워크 사용하여 Pharmacy repository 테스트 코드 작성
      - RestTemplate 등 실제 빈을 주입받아야 하기 때문에 통합 테스트(SpringBootTest)로 구현했다.

---
+++ src/test/groovy/com/ncookie/pharmacy_recommendation/pharmacy/repository/PharmacyRepositorySpec.groovy
@@ -0,0 +1,42 @@
+package com.ncookie.pharmacy_recommendation.pharmacy.repository
+
+import com.ncookie.pharmacy_recommendation.AbstractIntegrationContainerBaseTest
+import com.ncookie.pharmacy_recommendation.pharmacy.entity.Pharmacy
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.testcontainers.spock.Testcontainers
+import spock.lang.Specification
+
+// 상속받은 AbstractIntegrationContainerBaseTest에서 Specification을 상속 받고
+// @SpringBootTest 어노테이션을 적용했기 때문에 별도로 명시하지 않아도 된다.
+@Testcontainers
+@SpringBootTest
+class PharmacyRepositorySpec extends Specification {
+
+    @Autowired
+    private PharmacyRepository pharmacyRepository
+
+    def "PharmacyRepository save"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        def result = pharmacyRepository.save(pharmacy)
+
+        then:
+        result.getPharmacyAddress() == address
+        result.getPharmacyName() == name
+        result.getLatitude() == latitude
+        result.getLongitude() == longitude
+    }
+}
