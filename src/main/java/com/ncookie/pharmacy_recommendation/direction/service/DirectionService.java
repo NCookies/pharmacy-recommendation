a0bdc0df9df39e0a6f87a13197512b9f3db497bf
SeungWoo Ryu <swstar21c@gmail.com>
2024-12-30 21:41:07 +0900

    리팩토링 - Direction 관련 컨트롤러 및 서비스 수정
    - 컨트롤러 레이어에서 도메인에 대해 직접적으로 다루는 것은 좋지 않으므로 이에 대한 부분을 서비스 클래스로 옮겼다.

--- src/main/java/com/ncookie/pharmacy_recommendation/direction/service/DirectionService.java
+++ src/main/java/com/ncookie/pharmacy_recommendation/direction/service/DirectionService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -23,9 +24,10 @@ public class DirectionService {
     
     // 약국 최대 검색 개수
     private static final int MAX_SEARCH_COUNT = 3;
-
     // 반경 10km 이내에서만 약국 검색
     private static final double RADIUS_KM = 10.0;
+
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
 
     private final PharmacySearchService pharmacySearchService;
     private final KakaoCategorySearchService kakaoCategorySearchService;
@@ -41,9 +43,15 @@ public class DirectionService {
         return directionRepository.saveAll(directionList);
     }
 
-    public Direction findById(String encodedId) {
+    public String findDirectionUrlById(String encodedId) {
         Long decodedId = base62Service.decodeDirectionId(encodedId);
-        return directionRepository.findById(decodedId).orElse(null);
+
+        Direction direction = directionRepository.findById(decodedId).orElse(null);
+
+        String params = String.join(",", direction.getTargetPharmacyName(),
+                String.valueOf(direction.getTargetLatitude()), String.valueOf(direction.getTargetLongitude()));
+
+        return UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params).toUriString();
     }
 
     /**
